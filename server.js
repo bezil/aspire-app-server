4816381e48da76ad0cfc0f883e10b7752125896e
BASIL GEORGE <bezilonnet@gmail.com>
2021-12-13 07:33:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
 server.use(router);
 
 server.listen(port);
